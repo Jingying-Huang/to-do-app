aa838dc796f4e3a51bfa45102b06f450f93ca186
Jingying Huang <wphuang@ucdavis.edu>
2020-09-29 12:03:43 -0700

    fixed context related error

--- main.go
+++ main.go
@@ -1,22 +1,42 @@
 package main
 
 import (
+	"context"
 	"html/template"
 	"net/http"
 
+	"github.com/go-redis/redis/v8"
 	"github.com/gorilla/mux"
 )
 
+var client *redis.Client
 var templates *template.Template
 
 func main() {
+	client = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
 	templates = template.Must(template.ParseGlob("templates/*.html"))
 	r := mux.NewRouter()
-	r.HandleFunc("/", indexHandler).Methods("GET")
+	r.HandleFunc("/", indexGetHandler).Methods("GET")
+	r.HandleFunc("/", indexPostHandler).Methods("POST")
 	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "index.html", nil)
+func indexGetHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := context.TODO()
+	tasks, err := client.LRange(ctx, "tasks", 0, 10).Result()
+	if err != nil {
+		return
+	}
+	templates.ExecuteTemplate(w, "index.html", tasks)
+}
+
+func indexPostHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	ctx := context.TODO()
+	task := r.PostForm.Get("task")
+	client.LPush(ctx, "tasks", task)
+	http.Redirect(w, r, "/", 302)
 }
