392e0a5e36e0c1514747d51c1bcd7247130f322d
Jingying Huang <wphuang@ucdavis.edu>
2020-09-29 15:12:54 -0700

    fixed the error for router

--- main.go
+++ main.go
@@ -1,46 +1,17 @@
 package main
 
 import (
-	"html/template"
 	"net/http"
 
 	"github.com/Jingying-Huang/to-do-app/models"
-
-	"github.com/gorilla/mux"
+	"github.com/Jingying-Huang/to-do-app/routes"
+	"github.com/Jingying-Huang/to-do-app/utils"
 )
-
-var templates *template.Template
 
 func main() {
 	models.Init()
-	templates = template.Must(template.ParseGlob("templates/*.html"))
-	r := mux.NewRouter()
-	r.HandleFunc("/", indexGetHandler).Methods("GET")
-	r.HandleFunc("/", indexPostHandler).Methods("POST")
-	fs := http.FileServer(http.Dir("./static/"))
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
+	utils.LoadTemplates("templates/*.html")
+	r := routes.NewRouter()
 	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
-}
-
-func indexGetHandler(w http.ResponseWriter, r *http.Request) {
-	tasks, err := models.GetTasks()
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Internal server error"))
-		return
-	}
-	templates.ExecuteTemplate(w, "index.html", tasks)
-}
-
-func indexPostHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	task := r.PostForm.Get("task")
-	err := models.PostTask(task)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Internal server error"))
-		return
-	}
-	http.Redirect(w, r, "/", 302)
 }
---
+++ routes/routes.go
@@ -0,0 +1,40 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/Jingying-Huang/to-do-app/models"
+	"github.com/Jingying-Huang/to-do-app/utils"
+	"github.com/gorilla/mux"
+)
+
+func NewRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/", indexGetHandler).Methods("GET")
+	r.HandleFunc("/", indexPostHandler).Methods("POST")
+	fs := http.FileServer(http.Dir("./static/"))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
+	return r
+}
+
+func indexGetHandler(w http.ResponseWriter, r *http.Request) {
+	tasks, err := models.GetTasks()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal server error"))
+		return
+	}
+	utils.ExecuteTemplate(w, "index.html", tasks)
+}
+
+func indexPostHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	task := r.PostForm.Get("task")
+	err := models.PostTask(task)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal server error"))
+		return
+	}
+	http.Redirect(w, r, "/", 302)
+}
